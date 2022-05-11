cccd13426c0b79bdc24c4330433327ce4322b4e6
shahad <shahadottt@gmail.com>
2022-05-11 08:31:48 +0300

    Add project files.

---
+++ live/signalr/hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace live.signalr.hubs
+{
+    public class ChatHub : Hub
+    {
+        public void send(string name, string massage)
+        {
+            Clients.All.addNewMessageToPage(name, massage);
+        }
+    }
+}
